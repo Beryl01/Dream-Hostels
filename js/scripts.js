3814bdb7b79e6101efdb50c05349cc6753dc1122
Nabz <bnnabiswa@gmail.com>
2020-03-02 12:56:08 +0300

    add random changes

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+Pizza = function(type, size, crust, toppings, number, delivery){
+    this.type = type;
+    this.size = size;
+    this.crust = crust;
+    this.toppings = toppings;
+    this.delivery = delivery;
+    this.number = number;
+  }
